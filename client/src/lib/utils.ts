afb188c99d2e9f474a94901f783112f5517c0c60
DESKTOP-54NCMET\PC-InfoPlus <truongpv@infoplusvn.com>
2025-10-24 19:57:50 +0700

    update

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -12,7 +12,7 @@ export async function defaultFetcher({ queryKey }) {
 
   // Cho phép truyền cả path hoặc URL đầy đủ
   const url = path.startsWith("http") ? path : `${BASE_URL}${path}`;
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem("authToken");
 
   const res = await fetch(url, {
     headers: {
